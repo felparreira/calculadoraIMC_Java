670d97014531981feabed99989efa56eed9911b6
Felipe Parreira <107953342+felparreira@users.noreply.github.com>
2023-06-09 17:52:47 -0300

    Add files via upload

---
+++ CalculadoraIMC/src/main/java/com/mycompany/calculadoraimc/CalculadoraIMC.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadoraimc;
+
+/**
+ *
+ * @author OS
+ */
+public class CalculadoraIMC {
+
+    public static void main(String[] args) {
+        FormCalc fc = new FormCalc();
+        fc.setVisible(true);
+    }
+}
